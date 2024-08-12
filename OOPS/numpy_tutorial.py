043ae008b45391a0d785a09667fbd3cd1e383397
Luke1836 <codingluke2023@gmail.com>
2024-08-12 23:23:43 +0530

    Numpy - 2

--- OOPS/numpy_tutorial.py
+++ OOPS/numpy_tutorial.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-a = np.array([1,2,3,4,5])
+a = np.array([1,2,3,4,5], dtype="int32")
 b = np.array([[1.0,2.0,3.0], [4.0,5.0, 6.0]])
 #print(b.ndim)
-
+#print(b.shape, a.itemsize)
