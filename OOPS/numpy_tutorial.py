0155c0572b11d6e2c0dfd30c0c2444e7e40d8a27
Luke1836 <codingluke2023@gmail.com>
2024-08-13 07:30:54 +0530

    Numpy - 4

--- OOPS/numpy_tutorial.py
+++ OOPS/numpy_tutorial.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as r
 
 a = np.array([1,2,3,4,5], dtype="int32")
 b = np.array([[1.0, 2.0, 3.0, 8.0, 9.0], [4.0, 5.0, 6.0, 7.0, 10.0]])
@@ -8,3 +9,14 @@ print(b.shape, a.itemsize)
 print(b[: , 2])  All the rows in the third column
 print(b[0, :])  All the columns in the first row 
 print(b[0, 0::2]) For the first row , start from the first column, move two positions to the right, select that element. """
+
+""" c = np.zeros((2,5), dtype="int32")
+print(c)
+
+# Create an array with all eleements equal to 99
+d = np.full((4,4), 88)
+print(d) """
+
+print(np.random.rand(3,4))
+print(np.identity(4))
+print(np.random.randint(-2, 2, size=(4,4)))
