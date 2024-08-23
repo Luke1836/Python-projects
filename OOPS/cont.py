b5f01c94726a1a458094eaa3399a1ec657184294
Luke1836 <codingluke2023@gmail.com>
2024-08-23 20:31:48 +0530

    Lists -3

--- OOPS/cont.py
+++ OOPS/cont.py
@@ -5,3 +5,24 @@ print(square)
 
 indices = [[i,j] for i in square for j in square]
 print(indices)
+
+lst = [1,11,2,3,3,12,2,31,3,12,13,3,2]
+print(lst)
+
+# Removing repeating numbers
+lst = list(set(lst))
+print(lst)
+
+# Matrix operations
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10,11,12]
+]
+
+print(len(matrix)) # Number of rows
+print(len(matrix[0])) # Number of columns
+
+tranposed_matrix = [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
+print(tranposed_matrix)
